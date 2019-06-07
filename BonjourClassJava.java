2214e5cd621d631f47d285a4be9d60d2d7454d1c
angai <alainfngai@gmail.com>
2019-06-08 00:59:10 +0100

    ajout troisieme fichier classe Bonjour en Java

---
+++ BonjourClassJava.java
@@ -0,0 +1,11 @@
+/* */
+//
+// classe "Bonjour"
+public class Bonjour 
+{
+	// Methode principale "main"
+	public static void main (String[] args)
+	{
+		System.out.println("Bonjour tout le monde !!!");
+	}
+}
