b997a594e72f8d6a90acdf2ee70f82a0dd60d8b7
angai <alainfngai@gmail.com>
2019-06-08 00:50:44 +0100

    ajout deuxieme fichier code source vb.net

---
+++ codevbnet.vb
@@ -0,0 +1,10 @@
+'
+' @ ANNEE    : 2018 / 2019
+' @ ECOLE    : ESIAC
+' @ FILIERE  : GENIE INFORMATIQUE
+' @ OPTION   : GENIE LOGICIEL
+' @ MATIERE  : MINI PROJETS AVEC VB.NET
+' @ FICHE 01 : TP01 - EXERCICE 03
+' @ AUTEUR   : NGAI NYEMB Alain Fils
+' @ EMAIl    : alainfngai@gmail.com
+' @ Linkedin : https://www.linkedin.com/in/alain-fils-ngai-nyemb-84982714a/
